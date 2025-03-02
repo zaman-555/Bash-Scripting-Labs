886fa8effa1140792f48c1fb439f17f4cf2ca802
zaman-555 <mohammadzamanfi@gmail.com>
2025-03-02 02:24:01 +0200

    initial commit

---
+++ greeting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script print
+
+name=Farid
+country=Finland
+
+echo "Hello there, welcome to Bash scripting labs!"
+echo "Today is $(date)"
+
+echo "My name is $name and I am living in $country."
+
+echo "Enter your name:"
+read name
+echo "Moi, $name!"
